31befbd153d4f66323a846c40f9dda8f00ba34ee
piny4man <development@pinya.dev>
2026-04-15 22:29:52 +0200

    fix: formatting

--- src/terminal.rs
+++ src/terminal.rs
@@ -2,7 +2,7 @@ use std::io::{self, stdout};
 
 use crossterm::{
     execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
 };
 use ratatui::prelude::*;
 
